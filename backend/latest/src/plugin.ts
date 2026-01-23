0d4203916f5b1a5cd53502042a77a43e23d92da3
Martin Falconer <mcfalconer@gmail.com>
2026-01-23 08:36:06 -0600

    initial plugin setup

---
+++ backend/latest/src/plugin.ts
@@ -0,0 +1,34 @@
+import { BackendPlugins } from '@common/types';
+
+type Graphql = {
+  input: { type: 'echo'; echo: string } | { type: 'doubleEcho'; echo: string };
+  output: { type: 'echo'; echo: string } | { type: 'doubleEcho'; echo: string };
+};
+
+const plugins: BackendPlugins = {
+graphql_query: ({ store_id, input: inputUntyped }): Graphql['output'] => {
+    const input = inputUntyped as Graphql['input'];
+
+    switch (input.type) {
+      case 'echo':
+        return { type: 'echo', echo: input.echo };
+      case 'doubleEcho':
+        use_graphql({
+          query: `
+            query MyQuery($input: JSON = "", $pluginCode: String = "", $storeId: String = "") {
+              pluginGraphqlQuery(input: $input, pluginCode: $pluginCode, storeId: $storeId)
+            }
+        `,
+          variables: {
+            storeId: store_id,
+            input: { type: 'echo', echo: 'yow' },
+            pluginCode: 'plugin_examples',
+          },
+        });
+        return {
+          type: 'doubleEcho',
+          echo: input.echo,
+        };
+    }
+  },
+}
