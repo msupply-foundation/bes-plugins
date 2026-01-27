b947a78ad7a45af2b4ff7a2f8b468871a04b650c
Martin Falconer <mcfalconer@gmail.com>
2026-01-27 13:52:22 -0600

    working workflow for issueing stock

--- backend/latest/src/generated-types/graphql.ts
+++ backend/latest/src/generated-types/graphql.ts
@@ -20,6 +20,90 @@ export type Incremental<T> =
   | {
       [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never;
     };
+export type AllocateBatchOutboundShipmentMutationVariables = Types.Exact<{
+  storeId: Types.Scalars['String']['input'];
+  input: BatchOutboundShipmentInput;
+}>;
+
+export type AllocateBatchOutboundShipmentMutation = {
+  __typename?: 'Mutations';
+  batchOutboundShipment: {
+    __typename?: 'BatchOutboundShipmentResponse';
+    allocateOutboundShipmentUnallocatedLines?: Array<{
+      __typename?: 'AllocateOutboundShipmentUnallocatedLineResponseWithId';
+      id: string;
+      response:
+        | {
+            __typename: 'AllocateOutboundShipmentUnallocatedLineError';
+            error: { __typename?: 'RecordNotFound'; description: string };
+          }
+        | {
+            __typename: 'AllocateOutboundShipmentUnallocatedLineNode';
+            inserts: {
+              __typename?: 'InvoiceLineConnector';
+              nodes: Array<{
+                __typename?: 'InvoiceLineNode';
+                id: string;
+                numberOfPacks: number;
+                stockLine?: {
+                  __typename?: 'StockLineNode';
+                  batch?: string | null;
+                } | null;
+              }>;
+            };
+            updates: {
+              __typename?: 'InvoiceLineConnector';
+              nodes: Array<{ __typename?: 'InvoiceLineNode'; id: string }>;
+            };
+          };
+    }> | null;
+  };
+};
+
+export type BatchOutboundShipmentMutationVariables = Types.Exact<{
+  storeId: Types.Scalars['String']['input'];
+  input: BatchOutboundShipmentInput;
+}>;
+
+export type BatchOutboundShipmentMutation = {
+  __typename?: 'Mutations';
+  batchOutboundShipment: {
+    __typename?: 'BatchOutboundShipmentResponse';
+    insertOutboundShipments?: Array<{
+      __typename?: 'InsertOutboundShipmentResponseWithId';
+      id: string;
+      response:
+        | {
+            __typename: 'InsertOutboundShipmentError';
+            error:
+              | { __typename?: 'OtherPartyNotACustomer'; description: string }
+              | { __typename?: 'OtherPartyNotVisible'; description: string };
+          }
+        | { __typename: 'InvoiceNode'; id: string; invoiceNumber: number }
+        | { __typename: 'NodeError' };
+    }> | null;
+    insertOutboundShipmentUnallocatedLines?: Array<{
+      __typename?: 'InsertOutboundShipmentUnallocatedLineResponseWithId';
+      id: string;
+      response:
+        | {
+            __typename: 'InsertOutboundShipmentUnallocatedLineError';
+            error:
+              | { __typename?: 'ForeignKeyError'; description: string }
+              | {
+                  __typename?: 'UnallocatedLineForItemAlreadyExists';
+                  description: string;
+                }
+              | {
+                  __typename?: 'UnallocatedLinesOnlyEditableInNewInvoice';
+                  description: string;
+                };
+          }
+        | { __typename: 'InvoiceLineNode'; id: string };
+    }> | null;
+  };
+};
+
 export type NamesQueryVariables = Types.Exact<{
   storeId: Types.Scalars['String']['input'];
   filter?: Types.InputMaybe<NameFilterInput>;
--- backend/latest/src/plugin.ts
+++ backend/latest/src/plugin.ts
@@ -3,85 +3,247 @@ import { BackendPlugins } from '@common/types';
 import { IssueStockEndpointInput, IssueStockEndpointResponse } from './types';
 import namesQueryText from './graphql-texts/customer.graphql';
 import itemsQueryText from './graphql-texts/items.graphql';
+import outboundShipmentMutationText from './graphql-texts/batchOutboundShipment.graphql';
+import allocateOutboundShipmentMutationText from './graphql-texts/allocateOutboundShipment.graphql';
 import {
+  BatchOutboundShipmentMutation,
+  BatchOutboundShipmentMutationVariables,
+  UpdateOutboundShipmentStatusInput,
   ItemsQuery,
   ItemsQueryVariables,
   NamesQuery,
   NamesQueryVariables,
 } from './generated-types/graphql';
-
-const customerQuery = (variables: NamesQueryVariables): NamesQuery => {
-  return use_graphql({ query: namesQueryText, variables }) as NamesQuery;
-};
-
-const itemsQuery = (variables: ItemsQueryVariables): ItemsQuery => {
-  return use_graphql({ query: itemsQueryText, variables }) as ItemsQuery;
-};
+import { uuidv7 } from 'uuidv7';
 
 type Graphql = {
   input: IssueStockEndpointInput;
   output: IssueStockEndpointResponse;
 };
 
+const customerQuery = (variables: NamesQueryVariables): { result?: NamesQuery; customerError?: Graphql['output'] } => {
+  try {
+    const result = use_graphql({ query: namesQueryText, variables }) as NamesQuery;
+    if (!result || result.names.nodes.length < 1) {
+      return {
+        customerError: {
+          success: false,
+          message: `No customer found for filter: ${JSON.stringify(variables.filter)}`,
+        },
+      };
+    }
+    return { result };
+  } catch (error) {
+    return {
+      customerError: {
+        success: false,
+        message: `Error getting customer: ${error}`,
+      },
+    };
+  }
+};
+
+const itemsQuery = (variables: ItemsQueryVariables): { result?: ItemsQuery; itemsError?: Graphql['output'] } => {
+  try {
+    const result = use_graphql({ query: itemsQueryText, variables }) as ItemsQuery;
+    if (!result || result.items.totalCount < 1) {
+      return {
+        itemsError: {
+          success: false,
+          message: `No items found for items filter: ${variables.filter}`,
+        },
+      };
+    }
+    return { result };
+  } catch (error) {
+    return {
+      itemsError: {
+        success: false,
+        message: `Error getting items: ${error}`,
+      },
+    };
+  }
+};
+
+const batchOutboundShipmentQuery = (
+  variables: BatchOutboundShipmentMutationVariables
+): BatchOutboundShipmentMutation => {
+  return use_graphql({
+    query: outboundShipmentMutationText,
+    variables,
+  }) as BatchOutboundShipmentMutation;
+};
+
+const batchAllocateOutboundShipmentQuery = (
+  variables: BatchOutboundShipmentMutationVariables
+): BatchOutboundShipmentMutation => {
+  return use_graphql({
+    query: allocateOutboundShipmentMutationText,
+    variables,
+  }) as BatchOutboundShipmentMutation;
+};
+
 const plugins: BackendPlugins = {
   graphql_query: ({ store_id, input }): Graphql['output'] => {
     const inp = input as Graphql['input'];
 
-    log({ t: 'Message', storeId: store_id });
+    // --- if the following lines are left as they are, the first store in my data set
+    // --- has no stock, and will never issue stock. Also, isVisible = false for most stores
 
-    const { stores: activeStores } = get_active_stores_on_site();
+    // const { stores: activeStores } = get_active_stores_on_site();
+    // if (!activeStores || activeStores.length < 1) {
+    //   return { success: false, message: 'No active stores found' };
+    // }
+    // const issueingStore = activeStores[0];
 
-    log({ t: 'Customer Id', activeStores: activeStores[0] });
+    // log({ t: 'Issueing Store', issueingStore });
 
-    if (
-      !activeStores ||
-      activeStores.length < 1 ||
-      activeStores.at(0)?.store_row.is_disabled
-    ) {
-      return { success: true, message: 'No active stores found' };
-    }
+    const issuingStoreId = store_id;
 
-    const customerId = activeStores[0].store_row.id;
-
-    log({ t: 'Customer Id', customerId });
-
-    const customerQueryResult = customerQuery({
-      storeId: store_id,
+    const { result: customerQueryResult, customerError } = customerQuery({
+      storeId: issuingStoreId,
       filter: inp.customerFilter,
     });
 
-    if (!customerQueryResult || customerQueryResult.names.nodes.length < 1) {
+    if (customerError) return customerError;
+    if (!customerQueryResult) {
       return {
-        success: true,
-        message: `No customer found for filter: ${inp.customerFilter}`,
+        success: false,
+        message: `Error getting customer with filter of: ${inp.customerFilter}`,
       };
     }
 
-    const itemsQueryResult = itemsQuery({
-      storeId: store_id,
+    const customer = customerQueryResult.names.nodes[0];
+    const customerId = customerQueryResult.names.nodes[0].id;
+
+    // log({ t: 'Customer Store', customer });
+
+    const { result: itemsQueryResult, itemsError } = itemsQuery({
+      storeId: issuingStoreId,
       filter: inp.itemFilter,
     });
 
-    if (!itemsQueryResult || itemsQueryResult.items.totalCount < 1) {
+    if (itemsError) return itemsError;
+    if (!itemsQueryResult) {
       return {
-        success: true,
-        message: `No items found for items filter: ${inp.itemFilter}`,
+        success: false,
+        message: `Error getting item with filter of: ${inp.itemFilter}`,
       };
     }
 
-    const itemId = itemsQueryResult.items.nodes[0].id;
+    const foundItem = itemsQueryResult.items.nodes[0];
 
-    log({ t: 'item id', itemId });
+    log({ t: 'item', foundItem });
+
+    if (foundItem.availableStockOnHand < inp.quantity) {
+      return {
+        success: false,
+        message: `Not enough stock to fullfil order. Available stock for code ${foundItem.code}: ${foundItem.availableStockOnHand}.  Qty requested: ${inp.quantity}`,
+      };
+    }
+
+    log({ t: 'item id', id: foundItem.id });
+
+    const shipmentId = uuidv7();
+    const shipmentLineId = uuidv7();
+
+    // Create outbound shipment
+    const outboundShipmentInput: BatchOutboundShipmentMutationVariables = {
+      storeId: issuingStoreId,
+      input: {
+        insertOutboundShipments: [
+          {
+            id: shipmentId,
+            otherPartyId: customerId,
+          },
+        ],
+        insertOutboundShipmentUnallocatedLines: [
+          {
+            id: shipmentLineId,
+            invoiceId: shipmentId,
+            itemId: foundItem.id,
+            quantity: inp.quantity,
+          },
+        ],
+      },
+    };
+
+    try {
+      const insertOutboundShipmentUnallocatedResult = batchOutboundShipmentQuery(outboundShipmentInput);
+
+      if (!insertOutboundShipmentUnallocatedResult) {
+        // TODO: rollback the outboundshipment if required
+        throw Error(`Failed to issue the stock for item code: ${foundItem.code}, quantity: ${inp.quantity}`);
+      }
+    } catch (error) {
+      return {
+        success: true,
+        message: `${error}`,
+      };
+    }
+
+    // Allocate the outbound shipment lines
+
+    try {
+      const allocateOutboundShipmentInput: BatchOutboundShipmentMutationVariables = {
+        storeId: issuingStoreId,
+        input: {
+          allocatedOutboundShipmentUnallocatedLines: [shipmentLineId],
+        },
+      };
+
+      const allocateOutboundShipmentUnallocatedResult =
+        batchAllocateOutboundShipmentQuery(allocateOutboundShipmentInput);
+
+      log({ t: 'allocateResult', allocateOutboundShipmentUnallocatedResult });
+
+      if (!allocateOutboundShipmentUnallocatedResult) {
+        // TODO: rollback the outboundshipment
+        throw Error(
+          `Failed allocate lines to issue the stock for item code: ${foundItem.code}, quantity: ${inp.quantity}`
+        );
+      }
+    } catch (error) {
+      return {
+        success: true,
+        message: `${error}`,
+      };
+    }
+
+    // Update to 'shipped'
+    try {
+      const updateOutboundShipmentInput: BatchOutboundShipmentMutationVariables = {
+        storeId: issuingStoreId,
+        input: {
+          updateOutboundShipments: [
+            {
+              id: shipmentId,
+              status: UpdateOutboundShipmentStatusInput.Shipped,
+            },
+          ],
+        },
+      };
+
+      const updateOutboundShipmentResult = batchOutboundShipmentQuery(updateOutboundShipmentInput);
+
+      log({ t: 'updateResult', updateOutboundShipmentResult });
+
+      if (!updateOutboundShipmentResult) {
+        // TODO: rollback the outboundshipment
+        throw Error(
+          `Failed to update order to issue the stock for item code: ${foundItem.code}, quantity: ${inp.quantity}`
+        );
+      }
+    } catch (error) {
+      return {
+        success: true,
+        message: `${error}`,
+      };
+    }
 
     return {
       success: true,
-      message:
-        'I think this has worked! ' +
-        inp.quantity +
-        '   ' +
-        inp.customerFilter +
-        '   ' +
-        inp.itemFilter,
+      message: `Issued stock for store: ${customer.name}, item: ${foundItem.code}, quantity: ${inp.quantity}`,
     };
   },
 };
